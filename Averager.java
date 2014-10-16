87848b099e8fb165828315b674871cb105ae194e
JordanKoski <jordan_koski@asl.oeg>
2014-10-16 13:24:51 +0100

    Averager

--- Averager.java
+++ Averager.java
@@ -7,10 +7,26 @@
 *
 */
 
-public class Averager {
-	
-	public static void main(String[] args) {
-		// TODO - Put your code here
-	}
+import java.util.Scanner;
 
-}
+public class Averager {
+
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      double totalNumbers = 0;
+      double enterNumbers = 0;
+      System.out.println("Enter a number you would like to find the mean of.");
+      double addNumbers = scnr.nextDouble();
+         while (addNumbers != -1) { 
+            totalNumbers = totalNumbers + addNumbers;
+            enterNumbers = enterNumbers + 1;
+            System.out.println("Enter another number or -1 to exit");
+            addNumbers = scnr.nextDouble();
+            }
+            
+            System.out.println("Average of numbers: "+totalNumbers / enterNumbers);
+         
+         }
+ }         
+            
+           
